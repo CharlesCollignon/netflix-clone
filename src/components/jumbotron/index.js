aeb3e877d101e348019618b445922e52005af670
Charles Collignon <collignoncharles@gmail.com>
2021-08-30 17:27:34 +0200

    jumbotron init

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Container, Item, Inner } from './styles/jumbotron';
+import { 
+  Container, 
+  Item, 
+  Inner, 
+  Title,
+  SubTitle,
+  Image
+} from './styles/jumbotron';
 
 export default function Jumbotron({ children, direction = 'row', ...restProps }) {
   return (
@@ -11,4 +18,16 @@ export default function Jumbotron({ children, direction = 'row', ...restProps })
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />
 }
