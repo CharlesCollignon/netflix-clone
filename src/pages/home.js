376dff2cff297e6a92f9bcf18bb95fc891d35851
Charles Collignon <collignoncharles@gmail.com>
2021-08-30 16:21:14 +0200

    init styled-component

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Jumbotron from '../components/jumbotron'
+
+export default function Home() {
+  return (
+    <Jumbotron.Container>
+      <p>Well hello there !</p>
+    </Jumbotron.Container>
+
+  )
+}
